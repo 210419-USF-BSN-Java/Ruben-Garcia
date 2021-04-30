7e2303af8cd333575154f5acb1210e862183adbb
Ruben <rubengarcia0515@gmail.com>
2021-04-29 23:58:33 -0700

    revised generic dao

--- project0/src/main/java/daos/GenericDaos.java
+++ project0/src/main/java/daos/GenericDaos.java
@@ -1,5 +1,11 @@
 package daos;
 
-public interface GenericDaos {
+import java.util.List;
 
+public interface GenericDaos<T> {
+	public T add(T t); 
+	public T getById(Integer id); 
+	public List<T> getAll(); 
+	public Integer update(T t);
+	public Integer delete(T t); 
 }

10aa1524236c56ed5e3ecfdffb5c924be6b37179
Ruben <rubengarcia0515@gmail.com>
2021-04-30 00:03:30 -0700

    added all abstract user stories here for future ui

---
+++ project0/src/main/java/presentation/uiActions.java
@@ -0,0 +1,27 @@
+package presentation;
+
+public interface uiActions {
+	public abstract void logIn(); 
+	public abstract void logout();
+	public abstract void createUserAccount(); 
+	//on the fence about this, but whatever..
+	public abstract void whoseLoggedIn(); 
+	public abstract void exit(); 
+	//Done, further review 
+	public abstract void addItem(); 
+	
+	public abstract void offerDecision(); 
+	
+	public abstract void removeItem(); 
+	
+	public abstract void viewAllPayment();
+	
+    public abstract void terminate();
+
+    public abstract void viewSalesHistory(); 
+    
+
+    public abstract void createEmployeeAccount();
+
+    public abstract void fireEmployee(); 
+}

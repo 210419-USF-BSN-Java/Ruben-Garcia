ed44ad0e3e18cf37ab174d1d7509e45bda4cc984
Ruben <rubengarcia0515@gmail.com>
2021-05-15 15:17:15 -0700

    fixed db connection class, works now

--- project1/reimbusement-app/src/main/java/util/ConnectionUtil.java
+++ project1/reimbusement-app/src/main/java/util/ConnectionUtil.java
@@ -3,12 +3,16 @@ package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import org.apache.log4j.Logger;
+import java.sql.*; 
 
 //variables set up in sys env 
 public class ConnectionUtil {
-    private static Connection connection; 
+    private final static Logger log = Logger.getLogger(ConnectionUtil.class);
+    private static Connection connection;
+    
     //singleton pattern 
-    public static Connection getConnectionFromEnv() throws SQLException {
+    public static Connection getConnectionFromEnv() throws SQLException {	
 	String url = System.getenv("postgres_aws_ers_p1_db_url"); 
 	String username = System.getenv("postgres_aws_ers_p1_db_user"); 
 	String password = System.getenv("postgres_aws_ers_p1_db_password"); 
@@ -20,5 +24,38 @@ public class ConnectionUtil {
 	return connection; 
     }
     
+    public static Connection getHardCodedConnection() throws SQLException
+	{
+		String url = "com-revature-ers-p1.cl0hmzmpfzgp.us-east-2.rds.amazonaws.com";
+		String username = "postgres";
+		String password = "Noctis94.";
+
+		if(connection == null || connection.isClosed()) {
+		connection = DriverManager.getConnection(url, username, password);
+		}
+		
+		return connection;
+	}
+    
+    public static Connection getConnection() throws SQLException {
+    	try {
+    		Class.forName("org.postgresql.Driver"); 
+    	}catch(ClassNotFoundException e) {
+    		e.printStackTrace();
+    	}
+    	
+    	String RDS_HOSTNAME = "com-revature-ers-p1.cl0hmzmpfzgp.us-east-2.rds.amazonaws.com";
+		String username = "postgres";
+		String password = "Noctis94.";
+		String dbName = "postgres"; 
+		String jdbcURL = "jdbc:postgresql://" +RDS_HOSTNAME + ":" +5432 +"/" + dbName + "?user=" + username + "&password=" + password;
+		
+		if(connection == null || connection.isClosed()) {
+			connection = DriverManager.getConnection(jdbcURL);
+		}
+		
+		return connection;
+    }
+     
     
 }

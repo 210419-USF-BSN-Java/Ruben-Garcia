f9780f4b21ce64d646a9f20e3d35c80997323397
Ruben <rubengarcia0515@gmail.com>
2021-05-23 14:00:13 -0700

    added servlet for showing all resolved request

---
+++ project1/reimbusement-app/src/main/java/controller/ManagerShowResolvedRequestServlet.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import model.Reimbursement;
+import service.ManagerService;
+
+@WebServlet(value="/getAllResolvedRequests") 
+public class ManagerShowResolvedRequestServlet extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	ManagerService ms = new ManagerService(); 
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		List<Reimbursement> pendingRequest = ms.viewAllResolvedRequest();
+		ObjectMapper obj = new ObjectMapper();
+		String jsonFormat = obj.writeValueAsString(pendingRequest); 
+		PrintWriter out = response.getWriter(); 
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		out.print(jsonFormat);
+		out.flush();
+	}
+}

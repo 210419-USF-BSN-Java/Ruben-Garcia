8f250d42363028bd5612ff1222bd2ec73e0441cd
Ruben <rubengarcia0515@gmail.com>
2021-05-23 13:59:07 -0700

    added servlet for manager,view all resolved request

--- project1/reimbusement-app/src/main/java/controller/ManagerShowPendingRequestServlet.java
+++ project1/reimbusement-app/src/main/java/controller/ManagerShowPendingRequestServlet.java
@@ -1,31 +1,16 @@
 package controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.annotation.WebInitParam; 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.apache.tomcat.util.json.JSONParser;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-
 import model.Reimbursement;
-import model.User;
-import service.EmployeeServices;
 import service.ManagerService;
-import service.UserLoginService;
 
 @WebServlet(value="/getAllPendingRequests") 
 public class ManagerShowPendingRequestServlet extends HttpServlet {

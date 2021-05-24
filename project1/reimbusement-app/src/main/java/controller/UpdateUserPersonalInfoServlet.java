fc74f71de3f1e5c10d38e01ce51fa0d6ad657550
Ruben <rubengarcia0515@gmail.com>
2021-05-24 04:11:58 -0700

    skeleton page for manager dashboard with fetch methods attached to event listeners

--- project1/reimbusement-app/src/main/java/controller/UpdateUserPersonalInfoServlet.java
+++ project1/reimbusement-app/src/main/java/controller/UpdateUserPersonalInfoServlet.java
@@ -1,20 +1,10 @@
 package controller;
-import service.EmployeeServices;
-
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.stream.Collectors;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.annotation.WebInitParam; 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebServlet(value="/UpdateUserInfo")
 public class UpdateUserPersonalInfoServlet extends HttpServlet {
--- project1/reimbusement-app/src/main/webapp/static/manager_view.js
+++ project1/reimbusement-app/src/main/webapp/static/manager_view.js
@@ -1,7 +1,9 @@
 const getUserInfoButton = document.getElementById("get-user-info");
 
 getUserInfoButton.addEventListener('click', function(event){
+    //userloggedin
     console.log("clicked")
+    let userData;
     async function getInfo(){
          let data = await fetch("http://localhost:8080/reimbursement-app/getEmployeeInfo")
          .then(Response => Response.json())
@@ -29,3 +31,83 @@ logoutButton.addEventListener("click", function(event){
     }
     invalidate(); 
 })
+
+let employeeHeaders = ["First Name", "Last Name", "Username", "UserType"];
+tableDiv = document.getElementById("list-of-employees"); 
+getAllEmployeeList = document.getElementById("get-all-employees")
+getAllEmployeeList.addEventListener("click", function(event){
+    let list; 
+    console.log("get all employees endpoint fetched")
+    async function getInfo(){
+         let data = await fetch("http://localhost:8080/reimbursement-app/getAllEmployee")
+         .then(Response => Response.json())
+        try{
+            list = JSON.parse(data)
+        } catch(e){
+            list  = data
+        }
+
+        console.log(list)
+    }
+    getInfo();
+})
+
+getAllPendingRequests = document.getElementById("view-all-pending-requests")
+getAllPendingRequests.addEventListener("click", function(event){
+    let list; 
+    console.log("get all pending requests endpoint called")
+    async function getInfo(){
+         let data = await fetch("http://localhost:8080/reimbursement-app/getAllPendingRequests")
+         .then(Response => Response.json())
+        try{
+            list = JSON.parse(data)
+        } catch(e){
+            list  = data
+        }
+
+        console.log(list)
+    }
+    getInfo();
+})
+
+
+getAllResolvedRequests = document.getElementById("view-all-resolved-requests")
+getAllResolvedRequests.addEventListener("click", function(event){
+    let list; 
+    console.log("get all resolved requests endpoint called")
+    async function getInfo(){
+         let data = await fetch("http://localhost:8080/reimbursement-app/getAllResolvedRequests")
+         .then(Response => Response.json())
+        try{
+            list = JSON.parse(data)
+        } catch(e){
+            list  = data
+        }
+
+        console.log(list)
+    }
+    getInfo();
+})
+
+//fetch method for updating ticket
+/*
+updateTicketButton = document.getElementById("update-ticket-submit")
+getAllResolvedRequests.addEventListener("click", function(event){
+    let list; 
+    console.log("post request for updating a pending ticket issued")
+    async function getInfo(){
+         let data = await fetch("http://localhost:8080/reimbursement-app/reimbursement_resolve", {
+             bo
+             method: "Post", 
+             headers: {
+                 "Content-type": "application/json; charset=UTF-8"
+             }
+         }
+         )
+        
+
+        console.log(list)
+    }
+    getInfo();
+})
+*/

e91751052d5084bdf4e24d1b77f64e35c5aad47f
Ruben <rubengarcia0515@gmail.com>
2021-05-26 01:02:52 -0700

    log4j example

---
+++ project1/reimbusement-app/src/main/java/driver/Log4jHelloWorld.java
@@ -0,0 +1,17 @@
+package driver;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+ 
+public class Log4jHelloWorld {
+ 
+    static final Logger logger = Logger.getLogger(Log4jHelloWorld.class);
+ 
+    public static void main(String[] args)
+    {
+        //Configure logger
+        BasicConfigurator.configure();
+        logger.debug("Hello World!");
+    }
+}
+
